0e61922156b0fb754d8cb75519acbcd683f37cbc
ranamoataz3 <ranamoataz02@gmail.com>
2023-03-05 00:45:22 +0200

    Banner

--- src/assets/data/BannerImgs.js
+++
@@ -1,6 +0,0 @@
-import img1 from "../imgs/banner/bannerimg1.jpg";
-import img2 from "../imgs/banner/bannerimg2.jpg";
-
-const BannerImgs = [img1, img2];
-
-export default BannerImgs;
--- src/pages/home/homePage.js
+++ src/pages/home/homePage.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import classes from './home.module.css'
+import React from "react";
+import Banner from "./banner/Banner";
+import classes from "./home.module.css";
 
 const HomePage = () => {
   return (
     <div className={classes.container}>
-    
-        <h1 className={classes.title}> Welcome Home Y'all</h1>
-      
+      <h1 className={classes.title}> Welcome Home Y'all</h1>
+      <Banner />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
