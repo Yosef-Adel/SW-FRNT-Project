37dca7980f78d40c3741b661d73089641b3bda15
ranamoataz3 <ranamoataz02@gmail.com>
2023-03-13 14:01:30 +0200

    footer responsive

--- src/layouts/footer/Footer.js
+++ src/layouts/footer/Footer.js
@@ -2,23 +2,31 @@ import React from "react";
 import classes from "./footer.module.css";
 import FooterData from "../../assets/data/FooterData";
 
-/**
- * Component that renders nav bar
- *
- * @component
- */
 const Footer = (props) => {
   return (
     <div className={classes.footer}>
       <div className={classes.copyright}>
-        <div>© 2023 Envie</div>
+        <strong>© 2023 Envie</strong>
       </div>
       <div className={classes.footerdata}>
-        {FooterData.map((element) => (
+        {FooterData.staticData.map((element) => (
           <li className={classes.footeritem}>
             <div>{element}</div>
           </li>
         ))}
+      </div>
+
+      <div className={classes.dropdown}>
+        <select>
+          <option value="Egypt" selected>
+            <strong>Egypt</strong>
+          </option>
+          {FooterData.countriesData.map((element) => (
+            <option value={element} disabled>
+              {element}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   );
