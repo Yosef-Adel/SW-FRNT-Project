fa39e1e8c451bc60178f75732b249bbcea32a9d1
OmarrAlaa17 <102559077+OmarrAlaa17@users.noreply.github.com>
2023-03-15 00:19:37 +0200

    Event Card

--- src/generic components/event card/EventCard.js
+++ src/generic components/event card/EventCard.js
@@ -1,1 +1,31 @@
-import classes from './eventCard.css'
+import classes from './eventCard.module.css'
+import React from 'react'
+import {HiOutlineUser} from "react-icons/hi";
+
+const EventCard = (props) => {
+    return (
+      <div className={classes.card}>
+        <div className={classes.cardImage}>
+            <img src={props.img} alt="event_img" />
+        </div>   
+        <ul className={classes.cardContent}>
+            <h3>
+                {props.title}
+            </h3>
+            <li className={classes.time}>
+                {props.time}
+            </li>
+            <li className={classes.location}>
+                <p>{props.location}</p>
+                {props.price?<p>{props.price}</p>:null}
+            </li>
+            <li className={classes.company}>
+                <p>{props.companyName}</p>
+                <p> <HiOutlineUser size="13"/> {props.followersNo} followers</p>
+            </li>
+        </ul>         
+      </div>
+    );
+  };
+  
+  export default EventCard;
--- src/pages/home/homePage.js
+++ src/pages/home/homePage.js
@@ -2,9 +2,11 @@ import React from "react";
 import classes from "./home.module.css";
 import NavBar from "../../layouts/nav/NavBar";
 import Categories from "./categories/Categories";
-
+import EventCard from "../../generic components/event card/EventCard";
 import Banner from "./banner/Banner";
 import Footer from "../../layouts/footer/Footer";
+
+import eventImage1 from '../../assets/imgs/events/event2.png'
 
 const HomePage = () => {
   return (
@@ -12,6 +14,15 @@ const HomePage = () => {
       <NavBar />
       <Banner />
       <Categories/>
+      <EventCard 
+        img={eventImage1}
+        title="Celebrating Century : Presidency University"
+        time="Tue, Mar 14, 7:00 PM + 37 more events"
+        location="Presidency University, Kolkata"
+        price="Free"
+        companyName="Presidency University"
+        followersNo="100"
+      />
       <Footer />
     </div>
   );
