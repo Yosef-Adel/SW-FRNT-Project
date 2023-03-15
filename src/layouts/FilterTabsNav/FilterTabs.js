c1cbe589414cd371ee417bdf895f4e315520d806
Saykoyes <75132780+Saykoyes@users.noreply.github.com>
2023-03-16 00:54:37 +0200

    select_multfilters

--- src/layouts/FilterTabsNav/FilterTabs.js
+++ src/layouts/FilterTabsNav/FilterTabs.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState ,useEffect} from 'react'
 import classes from './FilterTabs.module.css'
 import FilterTabsData from '../../assets/data/FilterTabsData'
 import { NavLink } from 'react-router-dom'
 
-
 const FilterTabs= () => {
     const page =FilterTabsData.FilterTabsInfo;
-    const [clicked, setclicked] = useState(0);
-    console.log(page); 
-    
+    const [clicked, setIndexClicked] = useState(0);
+    //for selecting mulipule filters ---- to make it usestate of indexclicked([1,0,0,0,0,0]) if condition (clicked[index]=1)
+     function setclicked(index){
+        if(clicked[index]==0)
+        {
+            let clickedcl=clicked;
+            clickedcl[index]=1;
+            setIndexClicked(clicked => [...clicked, clickedcl]);
+        }
+    }
+       
+
     return(
         <div className={classes.FilterNav}>
             <div className={classes.Nav}>
@@ -16,10 +24,10 @@ const FilterTabs= () => {
                     {page[0].map((element,index) => {
                         return(
                             <div>
-                                <li className={`${classes.navItem}`} onClick={() => setclicked(index)}>
+                                <li className={`${classes.navItem}`} >
                                     <NavLink to={element.route}  activeClassName={classes.activeLink}>
                                     <div>
-                                        <div className={clicked==index? classes.clicked:classes.element}>{element.title}</div>
+                                        <div className={index==clicked? classes.clicked:classes.element} onClick={() =>setIndexClicked(index)}>{element.title}</div>
                                     </div>
                                     </NavLink>
                                 </li>
