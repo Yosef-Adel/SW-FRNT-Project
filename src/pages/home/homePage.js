127bf065207e30797df2fe86fa9c1ebd9c5dbc5f
ranamoataz3 <ranamoataz02@gmail.com>
2023-03-23 23:41:20 +0200

    routing & edit styling eventslist

--- src/pages/home/homePage.js
+++ src/pages/home/homePage.js
@@ -2,26 +2,26 @@ import React from "react";
 import classes from "./home.module.css";
 import NavBar from "../../layouts/nav/NavBar";
 import Categories from "./categories/Categories";
-import EventCard from "../../generic components/event card/EventCard";
+// import EventCard from "../../generic components/event card/EventCard";
 import Banner from "./banner/Banner";
 import Footer from "../../layouts/footer/Footer";
 import FilterTabs from "./filter_tabs/FilterTabs";
 import EventList from "../../generic components/Event List/EventList";
-import eventImage1 from '../../assets/imgs/events/event1.png';
-import eventImage2 from '../../assets/imgs/events/event2.png';
+// import eventImage1 from '../../assets/imgs/events/event1.png';
+// import eventImage2 from '../../assets/imgs/events/event2.png';
 
 const HomePage = () => {
   return (
     <div className={classes.container}>
       <NavBar />
       <Banner />
-      
+
       <div className={classes.containerbox}>
-        <FilterTabs/>
-        <Categories/>
-        <EventList/>
+        <FilterTabs />
+        <Categories />
+        <EventList />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
