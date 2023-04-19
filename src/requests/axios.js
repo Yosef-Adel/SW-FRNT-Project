936b349b5e265bf19e9aa576d2cb1de41616a47b
ranamoataz3 <ranamoataz02@gmail.com>
2023-04-19 15:20:47 +0200

    token pt 2

--- src/requests/axios.js
+++ src/requests/axios.js
@@ -2,26 +2,29 @@ import axios from "axios";
 
 let user = "";
 // To get the values of token and id from local storage
-if ((window.localStorage.getItem('persist:root')))
-{
-    user =JSON.parse(JSON.parse(window.localStorage.getItem('persist:root')).user);
-}
-else
-{
-    user = "";
+if (window.localStorage.getItem("persist:root")) {
+  user = JSON.parse(
+    JSON.parse(window.localStorage.getItem("persist:root")).user
+  );
+} else {
+  user = "";
 }
 
-const url = process.env.API_URL || "http://ec2-3-219-197-102.compute-1.amazonaws.com/"
+const url =
+  process.env.API_URL || "http://ec2-3-219-197-102.compute-1.amazonaws.com/";
 
 let instance = "";
-if (user){
-    instance  = axios.create({
-        baseURL: url,
-        headers: {
-            Authorization: "Bearer "+ JSON.parse(JSON.parse(window.localStorage.getItem('persist:root')).user).token,
-            ID: user.id
-        }
-    })
-}
 
-export default instance
+  instance = axios.create({
+    baseURL: url,
+    headers: {
+      Authorization:
+        "Bearer " +
+        JSON.parse(JSON.parse(window.localStorage.getItem("persist:root")).user)
+          .token,
+      ID: user.id,
+    },
+  });
+
+
+export default instance;
