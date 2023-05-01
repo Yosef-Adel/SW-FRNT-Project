363e8d52ad81318833af6608266a93466d35564a
DoniaRemah <doniaremah@gmail.com>
2023-05-02 01:23:23 +0300

    Solved Refresh Problem

--- src/requests/axios.js
+++ src/requests/axios.js
@@ -1,29 +1,35 @@
 import axios from "axios";
 
 
-let user = "";
-// To get the values of token and id from local storage
-if (window.localStorage.getItem("persist:root")) {
-  user = JSON.parse(
-    JSON.parse(window.localStorage.getItem("persist:root")).user
-  );
-} else {
-  user = "";
-}
+// let user = "";
+// // To get the values of token and id from local storage
+// if (window.localStorage.getItem("persist:root")) {
+//   user = JSON.parse(
+//     JSON.parse(window.localStorage.getItem("persist:root")).user
+//   );
+// } else {
+//   user = "";
+// }
 
-console.log(sessionStorage.getItem("token"))
+// console.log(sessionStorage.getItem("token"))
 
 const url =
   process.env.API_URL || "http://ec2-3-219-197-102.compute-1.amazonaws.com/";
 
 let instance = "";
-if (user) {
+if (JSON.parse(
+  JSON.parse(window.localStorage.getItem("persist:root")).user
+)) {
   instance = axios.create({
     baseURL: "https://sw-backend-project.vercel.app/",
     headers: {
       Authorization:
-        "Bearer " + user.token,
-      ID: user.id,
+        "Bearer " + JSON.parse(
+          JSON.parse(window.localStorage.getItem("persist:root")).user
+        ).token,
+      ID: JSON.parse(
+        JSON.parse(window.localStorage.getItem("persist:root")).user
+      ).id,
     },
   });
 }
