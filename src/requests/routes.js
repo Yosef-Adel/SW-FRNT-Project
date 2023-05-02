69de78bbdf67603d3a83049e6e6e40d0bde9b51e
ranamoataz3 <ranamoataz02@gmail.com>
2023-05-03 00:58:15 +0300

    Integration dashboard pt1

--- src/requests/routes.js
+++ src/requests/routes.js
@@ -1,14 +1,17 @@
 const routes = {
-    signUp: "/auth/sign-up",
-    logIn: "/auth/login",
-    events: "/api/events",
-    getUser: "/user",
-    userToCreator : "/user/to-creator",
-    promocode:"/promocode",
-    forgotPassword: "/auth/forgot-password",
-    creatorToUser : "/user/to-attendee",
-    resetPassword:"/auth/reset-password",
-    tickets:"/ticket",
-    placeOrder: "/order",
-}
-export default routes 
+  signUp: "/auth/sign-up",
+  logIn: "/auth/login",
+  events: "/api/events",
+  getUser: "/user",
+  userToCreator: "/user/to-creator",
+  promocode: "/promocode",
+  forgotPassword: "/auth/forgot-password",
+  creatorToUser: "/user/to-attendee",
+  resetPassword: "/auth/reset-password",
+  tickets: "/ticket",
+  placeOrder: "/order",
+  eventSoldTickets: "/getTicketsSoldForEvent",
+  eventSalesSummary: "/getSalesSummaryReport",
+  eventURL: "/getEventUrl",
+};
+export default routes;
