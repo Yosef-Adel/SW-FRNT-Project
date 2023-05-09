862e2930aa2d43960fa37f5aef6877eac2c5f040
ranamoataz3 <ranamoataz02@gmail.com>
2023-05-09 16:00:11 +0300

    promocodessss

--- src/pages/CreatorEventDetails/creatorTickets/tickestPromocodes/promocodesListView/PromoCodesList.js
+++ src/pages/CreatorEventDetails/creatorTickets/tickestPromocodes/promocodesListView/PromoCodesList.js
@@ -33,79 +33,81 @@ const PromoCodesList = ({ eventID }) => {
     <div
       id="CreatorTicketsPagePromoCodesContainer"
       className={classes.container}>
-      <div
-        id="CreatorTicketsPagePromoCodesTableContainer"
-        className={classes.salestable}>
-        <table id="CreatorTicketsPagePromoCodesTable">
-          <thead id="CreatorTicketsPagePromoCodesTableHead">
-            <tr
-              className={classes.tableheader}
-              id="CreatorTicketsPagePromoCodesTableHeadRow">
-              {tableheader.header.map((item, index) => {
+      {promocodes.length !== 0 ? (
+        <div
+          id="CreatorTicketsPagePromoCodesTableContainer"
+          className={classes.salestable}>
+          <table id="CreatorTicketsPagePromoCodesTable">
+            <thead id="CreatorTicketsPagePromoCodesTableHead">
+              <tr
+                className={classes.tableheader}
+                id="CreatorTicketsPagePromoCodesTableHeadRow">
+                {tableheader.header.map((item, index) => {
+                  return (
+                    <td
+                      key={"CreatorTicketsPagePromoCodesTableHeadData" + index}
+                      id={"CreatorTicketsPagePromoCodesTableHeadData" + index}
+                      className={classes.tableheaderitem}>
+                      {item}
+                    </td>
+                  );
+                })}
+                <td className={classes.tableheaderitemempty}></td>
+              </tr>
+            </thead>
+            <tbody id="CreatorTicketsPagePromoCodesTableBody">
+              {promocodes.map((item, index) => {
                 return (
-                  <td
-                    key={"CreatorTicketsPagePromoCodesTableHeadData" + index}
-                    id={"CreatorTicketsPagePromoCodesTableHeadData" + index}
-                    className={classes.tableheaderitem}>
-                    {item}
-                  </td>
+                  <tr
+                    key={"CreatorTicketsPagePromoCodesTableBodyRow" + index}
+                    id={"CreatorTicketsPagePromoCodesTableBodyRow" + index}
+                    className={classes.tablebodyrow}>
+                    <td id="CreatorTicketsPagePromoCodesTableBodypromoname">
+                      {item.name}
+                    </td>
+                    <td id="CreatorTicketsPagePromoCodesTableBodypromotype">
+                      Applies discount
+                    </td>
+                    <td id="CreatorTicketsPagePromoCodesTableBodypromodiscount">
+                      {item.amountOff == -1
+                        ? item.percentOff + " %"
+                        : "E£ " + item.amountOff}
+                    </td>
+                    <td id="CreatorTicketsPagePromoCodesTableBodypromouses">
+                      {item.used + " / " + item.limit}
+                    </td>
+                    <td id="CreatorTicketsPagePromoCodesTableBodypromostatus">
+                      <div className={classes.status}>
+                        <div
+                          className={
+                            now.diff(moment(item.endDate)) > 0
+                              ? classes.statusicon
+                              : classes.statusiconactive
+                          }></div>
+                        {now.diff(moment(item.endDate)) > 0 ? (
+                          <div className={classes.statusdesc}>
+                            <div>Ended</div>
+                            <div className={classes.statustitle}>
+                              Event has ended
+                            </div>
+                          </div>
+                        ) : (
+                          <div className={classes.statusdesc}>
+                            <div>Active</div>
+                            <div className={classes.statustitle}>
+                              Ends:{moment(item.endDate).format("ll")}
+                            </div>
+                          </div>
+                        )}
+                      </div>
+                    </td>
+                  </tr>
                 );
               })}
-              <td className={classes.tableheaderitemempty}></td>
-            </tr>
-          </thead>
-          <tbody id="CreatorTicketsPagePromoCodesTableBody">
-            {promocodes.map((item, index) => {
-              return (
-                <tr
-                  key={"CreatorTicketsPagePromoCodesTableBodyRow" + index}
-                  id={"CreatorTicketsPagePromoCodesTableBodyRow" + index}
-                  className={classes.tablebodyrow}>
-                  <td id="CreatorTicketsPagePromoCodesTableBodypromoname">
-                    {item.name}
-                  </td>
-                  <td id="CreatorTicketsPagePromoCodesTableBodypromotype">
-                    Applies discount
-                  </td>
-                  <td id="CreatorTicketsPagePromoCodesTableBodypromodiscount">
-                    {item.amountOff == -1
-                      ? item.percentOff + " %"
-                      : "E£ " + item.amountOff}
-                  </td>
-                  <td id="CreatorTicketsPagePromoCodesTableBodypromouses">
-                    {item.used + " / " + item.limit}
-                  </td>
-                  <td id="CreatorTicketsPagePromoCodesTableBodypromostatus">
-                    <div className={classes.status}>
-                      <div
-                        className={
-                          now.diff(moment(item.endDate)) > 0
-                            ? classes.statusicon
-                            : classes.statusiconactive
-                        }></div>
-                      {now.diff(moment(item.endDate)) > 0 ? (
-                        <div className={classes.statusdesc}>
-                          <div>Ended</div>
-                          <div className={classes.statustitle}>
-                            Event has ended
-                          </div>
-                        </div>
-                      ) : (
-                        <div className={classes.statusdesc}>
-                          <div>Active</div>
-                          <div className={classes.statustitle}>
-                            Ends:{moment(item.endDate).format("ll")}
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+            </tbody>
+          </table>
+        </div>
+      ) : null}
     </div>
   );
 };
